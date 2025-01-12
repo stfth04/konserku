8b89bdc5f6e5d330480a9cea4be582a9fa2f36cb
stfth04 <stfth12@gmail.com>
2025-01-12 15:10:57 +0700

    admin/manage.php

--- admin/manage.php
+++ admin/manage.php
@@ -61,7 +61,7 @@ $dashboard_events = $stmt->fetchAll();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Manage Event</title>
   <script src="https://cdn.tailwindcss.com"></script>
  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
 </head>
